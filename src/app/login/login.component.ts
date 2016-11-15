f630999caa76a9a6c17a67f81bfc66567a285e40
muhammad ali umer <ilaumer@yahoo.com>
2016-11-15 12:13:29 +0000

    remvoed initialization from login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -21,7 +21,7 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() {
     // reset login status
-    this.loginService.logout();
+    //this.loginService.logout();
   }
 
   login(): void {
