35be95e8f8f8d0e32b15a7cb9e7aa1a242fd52ea
Manoj Bisht <manabshy@gmail.com>
2016-11-21 16:39:47 +0000

    Taken out console comments

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -19,10 +19,10 @@ export class LoginService {
   constructor(private _http: Http) {}
 
   getUsers (): Observable<User[]> {
-    console.log('in getUsers');
+    //console.log('in getUsers');
     return this._http.get(this._userDataUrl)
                     .map(this.extractData)
-                    .do(data => console.log('Hola in Login service data' + data)) // eyeball results in the console
+                    //.do(data => console.log('In Login service data' + data)) // eyeball results in the console
                     .catch(this.handleError);
   }
 
@@ -32,23 +32,23 @@ export class LoginService {
         this.extractData(response);
         return this.Authenticate(user);
       })
-      .do(data => console.log('data in loginService:' + JSON.stringify(data)))
+      //.do(data => console.log('data in loginService:' + JSON.stringify(data)))
       ._catch(this.handleError);
 
   }
 
   Authenticate(user:User): Boolean{
-    console.log("In Authenticate"  + this._users);
+    //console.log("In Authenticate"  + this._users);
 
     let LoggedInUser = this._users.find(u => u.email === user.email && u.password === user.password);//
     if (LoggedInUser) {
-      console.log('LoggedInUser:' + LoggedInUser);
+      //console.log('LoggedInUser:' + LoggedInUser);
       this.isLoggedIn = true;
       localStorage.setItem('userName', LoggedInUser.firstName + ' ' + LoggedInUser.lastName);
       return true;
     } else {
       this.isLoggedIn = false;
-      console.log('LoggedInUser:' + LoggedInUser);
+     //console.log('LoggedInUser:' + LoggedInUser);
       return false;
     }
   }
@@ -59,15 +59,15 @@ export class LoginService {
       throw new Error('Bad response status: ' + res.status);
     }
     let body = res.json();
-     console.log('body:' + body );
+     //console.log('body:' + body );
      this._users = <User[]> res.json();
     return body.data || { };
   }
 
   private handleError(error: any) {
-    console.log(error);
+    //console.log(error);
     let errMsg = error.message || 'Server error';
-    console.error(errMsg); // log to console instead
+    //console.error(errMsg); // log to console instead
     return Observable.throw(errMsg);
     //return Observable.throw('server error');
   }
