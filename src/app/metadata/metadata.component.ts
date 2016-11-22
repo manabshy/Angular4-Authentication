daeec6cdd1d19caf33a7a16ad648a5fa88a8c447
muhammad ali umer <ilaumer@yahoo.com>
2016-11-22 11:40:03 +0000

    fixed the metadata and file upload component to use eventemitter

--- src/app/metadata/metadata.component.ts
+++ src/app/metadata/metadata.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder,FormsModule } from '@angular/forms';
 import { MetaDataModel } from "./metadata.model";
 
@@ -7,25 +7,7 @@ import { MetaDataModel } from "./metadata.model";
   templateUrl: './metadata.component.html',
   styleUrls: ['./metadata.component.css']
 })
-export class MetadataComponent implements OnInit {
+export class MetadataComponent {
   @Input() model: MetaDataModel;
-
-  metaDataForm: FormGroup;
-
-  constructor(private _formBuilder: FormBuilder) { }
-
-  ngOnInit() {
-    // let model = new MetaDataModel("cust123", "angular app", "jpeg", new Date(12/10/2016), new Date(18/11/2016));
-
-    this.metaDataForm = this._formBuilder.group({
-      "customerId": new FormControl(this.model.customerId),
-      "sourceSystem" : new FormControl(this.model.sourceSystem),
-      "contentType": new FormControl(this.model.contentType),
-      "receivedDate": new FormControl(this.model.receivedDate),
-      "uploadDate": new FormControl(this.model.uploadDate)
-    });
-
-  }
-
 }
 
