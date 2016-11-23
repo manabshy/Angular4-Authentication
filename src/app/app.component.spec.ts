1073e5380f9c32ad9daac1f6a9f2a6395bc94552
Manoj Bisht <manabshy@gmail.com>
2016-11-23 12:11:08 +0000

    Replaced button with Anchor routerLink for logout

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,87 +13,40 @@ import { LoginComponent }           from './login/login.component';
 import { HomeComponent }             from './home/home.component';
 import { FileUploadComponent }      from './file-upload/file-upload.component';
 import { MetadataComponent} from './metadata/metadata.component';
-
+import { LoginService } from './login/login.service';
 import { RouterLinkStubDirective }   from './testing/router-stubs';
 import { RouterOutletStubComponent } from './testing/router-stubs';
+import { Router } from '@angular/router';
 
 import { FormGroup, FormControl, Validators, FormBuilder, FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 @Component({selector: ' app-fileupload', template: ''})
   class HomeStubComponent {}
 
-
-let comp:    AppComponent;
-let fixture: ComponentFixture<AppComponent>;
+let comp:    HomeComponent;
+let fixture: ComponentFixture<HomeComponent>;
 
 describe('AppComponent & TestModule', () => {
   beforeEach( async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent,
-        LoginComponent, HomeComponent,FileUploadComponent,MetadataComponent,
-        RouterLinkStubDirective, RouterOutletStubComponent
+        LoginComponent, HomeComponent,FileUploadComponent,
+        RouterLinkStubDirective, RouterOutletStubComponent,MetadataComponent
       ],
-      providers:[FormBuilder],
+      providers:[FormBuilder,{provide:LoginService},{provide:Router}],
       imports:[FormsModule]
     })
+    
     .compileComponents()
     .then(() => {
-      fixture = TestBed.createComponent(AppComponent);
+      fixture = TestBed.createComponent(HomeComponent);
       comp    = fixture.componentInstance;
     });
   }));
   tests();
 });
-//////// Testing w/ NO_ERRORS_SCHEMA //////
-describe('AppComponent & NO_ERRORS_SCHEMA', () => {
-  beforeEach( async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AppComponent, RouterLinkStubDirective ],
-      schemas:      [ NO_ERRORS_SCHEMA ]
-    })
-    .compileComponents()
-    .then(() => {
-      fixture = TestBed.createComponent(AppComponent);
-      comp    = fixture.componentInstance;
-    });
-  }));
- // tests();
-});
-//////// Testing w/ real root module //////
-// Tricky because we are disabling the router and its configuration
-// Better to use RouterTestingModule
-import { AppModule }    from './app.module';
-import { AppRoutingModule }    from './routing/routing.component';
 
-describe('AppComponent & AppModule', () => {
-
-  beforeEach( async(() => {
-
-    TestBed.configureTestingModule({
-      imports: [ AppModule ]
-    })
-
-    // Get rid of app's Router configuration otherwise many failures.
-    // Doing so removes Router declarations; add the Router stubs
-    .overrideModule(AppModule, {
-      remove: {
-        imports: [ AppRoutingModule ]
-      },
-      add: {
-        declarations: [ RouterLinkStubDirective, RouterOutletStubComponent ]
-      }
-    })
-
-    .compileComponents()
-
-    .then(() => {
-      fixture = TestBed.createComponent(AppComponent);
-      comp    = fixture.componentInstance;
-    });
-  }));
-//  tests();
-});
 
 function tests() {
   let links: RouterLinkStubDirective[];
@@ -105,20 +58,24 @@ function tests() {
 
     // find DebugElements with an attached RouterLinkStubDirective
     linkDes = fixture.debugElement
-      .queryAll(By.directive(RouterLinkStubDirective));
+      .queryAll(By.directive(RouterLinkStubDirective));  
 
     // get the attached link directive instances using the DebugElement injectors
     //console.log('linkDes:' + linkDes);
     links = linkDes
       .map(de => de.injector.get(RouterLinkStubDirective) as RouterLinkStubDirective);
+
+    // find buttons
+    const buttons    = fixture.debugElement.queryAll(By.css('button'));
+   // this.logBtn = buttons[0];
   });
   
   it('can instantiate it', () => {
       expect(comp).not.toBeNull();
     });
- 
-  it('can get RouterLinks from template', () => {
-    //console.log('links.length:' + + links.length);
-    expect(links.length).toBe(0, 'should have No links as there are No links at present');
-  });
+  
+  it('can get RouterLinks from HomeComponent template', () => {
+    console.log('links.length:' +  links.length);
+    expect(links.length).toBe(1, 'should have No links as there are No links at present');
+  });  
 }
