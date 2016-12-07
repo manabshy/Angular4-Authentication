5bdb0530bec8a8a2aa7886ec976ae7f22e824c3e
muhammad ali umer <ilaumer@yahoo.com>
2016-12-07 09:35:58 +0000

    file upload component table to add selected files

--- src/app/shared/aside/aside.component.ts
+++ src/app/shared/aside/aside.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-aside',
   templateUrl: './aside.component.html',
-  styleUrls: ['./aside.component.css']
+  styleUrls: ['./aside.component.css'],
 })
-export class AsideComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class AsideComponent {
 
 }
