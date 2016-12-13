52acc232b62e98f2590b6eac7f5f703a5cbe274f
Manoj Bisht <manabshy@gmail.com>
2016-12-13 17:24:27 +0000

    updated app config file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,16 +4,16 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class AppConfigs {
 
-	constructor() { }
+    constructor() { }
 
-	private useMockData:boolean = true;
-	private mockUrl: string = "http://localhost:8090";
+    private useMockData:boolean = true;
+    private root: string = (this.useMockData) ? 'http://localhost:8090' : 'http://172.19.32.126:8080';
 
-	//upload url
-	public uploadUri:string  = ((this.useMockData) ? this.mockUrl : "http://172.19.32.126:8080") + '/ingestion-service-web/igs/document/upload';
+    // upload url 
+    public uploadUri:string  = this.root + '/ingestion-service-web/igs/document/upload';
 
-	//file upload end point
-	public updateUri:string =  ((this.useMockData) ? this.mockUrl : "http://172.19.32.126:8081") + '/ingestion-service-web/igs/document/update';
-
+    // file upload end point 
+    public updateUri:string    =  this.root + '/ingestion-service-web/igs/document/update/';
 
 }
+
